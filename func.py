d2665d5eebd9b9b164f3dafb3ed3ff08acc1bf5e
sepandhaghighi <sepand.haghighi@yahoo.com>
2015-05-30 10:15:20 -0700

    Repeat Code Added

--- func.py
+++ func.py
@@ -1,12 +1,13 @@
 from operator import *
+# Error Check And Correction Functions In Python
 def parity_gen(error_object):
         '''
 parity_gen(error_object)-> String
 '''
         temp=0
-        length=len(error_object.str)
+        length=len(error_object)
         for i in range(length):
-            temp=xor(int(error_object.str[i]),temp)
+            temp=xor(int(error_object[i]),temp)
         output=error_object.str+str(temp)
         return output
 def parity_det(error_object):
@@ -14,11 +15,35 @@ def parity_det(error_object):
 parity_det(error_object)-> Boolean
 '''
         temp=0
-        length=len(error_object.str)
+        length=len(error_object)
         for i in range(length-1):
-            temp=xor(int(error_object.str[i]),temp)
-        if str(temp)==error_object.str[-1]:
+            temp=xor(int(error_object[i]),temp)
+        if str(temp)==error_object[-1]:
                 return True
         else:
                 return False
+def repeat_gen(error_object):
+        '''
+repeat_gen(error_object) -> String
+'''
+
+        return error_object.str*error_object.flag
+def repeat_det(error_object):
+        '''
+repeat_det(error_object) -> Boolean
+'''
+        temp=None
+        length=len(error_object)
+        repeat_number=int(error_object.flag)
+        message_length=length//repeat_number
+        original_message=error_object[0:message_length]
+        for i in range(repeat_number):
+                temp=error_object[i*message_length:(i+1)*message_length]
+                if temp!=original_message:
+                        return False
+        return True
+        
+               
+        
+
         
--- main.py
+++ main.py
@@ -4,28 +4,36 @@ def convert_gen(error_object):
     Convert_gen(error_object) -> error_object
 '''
     if error_object.code=="Parity":
-        return error_detect(parity_gen(error_object),error_object.code)
+        return error_detect(parity_gen(error_object),error_object.code,error_object.flag)
+    elif error_object.code=="Repeat":
+        return error_detect(repeat_gen(error_object),error_object.code,error_object.flag)
 def convert_det(error_object):
     '''
     Convert_det(error_object)-< Boolean
 '''
     if error_object.code=="Parity":
         return parity_det(error_object)
+    elif error_object.code=="Repeat":
+        return repeat_det(error_object)
         
 class error_detect:
  
     '''
             (String , Error Detection Method)-> Error Detection Object)
     '''  
-    def __init__(self,string,method):
+    def __init__(self,string,method,flag=2):
        
         self.str=string
         self.code=method
-
+        self.flag=flag
         
+    def __getitem__(self,i):
+        return self.str[i]
+    def __len__(self):
+        return len(self.str)    
         
     def __str__(self):
 
-        return ("Error_Detection("+self.str+","+str(self.code)+")")
+        return ("Error_Detection("+self.str+","+str(self.code)+","+str(self.flag)+")")
     
 
